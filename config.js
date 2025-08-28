6fa9ff45a92ac927d7d547bce50b1a39b92beb5e
diagNOS-it <scott@diagnos.it.com>
2025-08-28 20:59:56 +1200

    Update config.js
    Updating privacy and terms url for app

--- config.js
+++ config.js
@@ -3,4 +3,6 @@ const CONFIG = {
   companyName: "Elephant Technologies",
   email: "dev@elephnt.tech",
   domain: "https://elephnt.tech",
+  diagnosprivacyPolicyUrl: "https://raw.githubusercontent.com/diagNOS-it/diagNOS_policies/refs/heads/main/policies/privacy_policy.html",
+  diagnostermsOfServiceUrl: "https://raw.githubusercontent.com/diagNOS-it/diagNOS_policies/refs/heads/main/policies/terms_policy.html"
 };
