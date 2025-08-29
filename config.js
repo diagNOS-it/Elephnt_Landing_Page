aaff036a5ba5f9c41e5acb0c15d55a8c68dad6d5
diagNOS-it <scott@diagnos.it.com>
2025-08-29 12:06:25 +1200

    Update config.js CORS

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ const CONFIG = {
   domain: "https://elephnt.tech",
   diagnosprivacyPolicyUrl: "https://raw.githubusercontent.com/diagNOS-it/diagNOS_policies/refs/heads/main/policies/privacy_policy.html",
   diagnostermsOfServiceUrl: "https://raw.githubusercontent.com/diagNOS-it/diagNOS_policies/refs/heads/main/policies/terms_policy.html",
-  privacyPolicyUrl: "https://raw.githubusercontent.com/diagNOS-it/Elephnt_Policies/refs/heads/main/policies/privacy_policy.html",
+  privacyPolicyUrl: "https://cdn.jsdelivr.net/gh/diagNOS-it/Elephnt_Policies@main/policies/privacy_policy.html",
   mission: "To provide real-world solutions to real-world problems in innovative ways. We strive to be efficient and sustainable."
 };
