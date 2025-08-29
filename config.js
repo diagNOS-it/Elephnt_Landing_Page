b7633bae05981b4c32bbb9cc1e1b7e1f4dd9b862
diagNOS-it <scott@diagnos.it.com>
2025-08-29 12:01:26 +1200

    Update config.js with privacy, mission

--- config.js
+++ config.js
@@ -4,5 +4,7 @@ const CONFIG = {
   email: "dev@elephnt.tech",
   domain: "https://elephnt.tech",
   diagnosprivacyPolicyUrl: "https://raw.githubusercontent.com/diagNOS-it/diagNOS_policies/refs/heads/main/policies/privacy_policy.html",
-  diagnostermsOfServiceUrl: "https://raw.githubusercontent.com/diagNOS-it/diagNOS_policies/refs/heads/main/policies/terms_policy.html"
+  diagnostermsOfServiceUrl: "https://raw.githubusercontent.com/diagNOS-it/diagNOS_policies/refs/heads/main/policies/terms_policy.html",
+  privacyPolicyUrl: "https://raw.githubusercontent.com/diagNOS-it/Elephnt_Policies/refs/heads/main/policies/privacy_policy.html",
+  mission: "To provide real-world solutions to real-world problems in innovative ways. We strive to be efficient and sustainable."
 };
